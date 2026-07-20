58e9acc6b8f5e57fa0f8f4e9f88ffe46be0ad118
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-20 13:55:18 -0500

    added click functionality to all menus

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -8,6 +8,10 @@ import NavItems from "./NavItems";
 const MobileSidebar = () => {
   let sidebar: SidebarComponent;
 
+  const toggleSidebar = () => {
+    sidebar.toggle();
+  };
+
   return (
     <div className="mobile-sidebar wrapper">
       <header>
@@ -16,19 +20,19 @@ const MobileSidebar = () => {
           <h1>Tourvisto</h1>
         </Link>
 
-        <button onClick={() => sidebar.toggle()}>
+        <button onClick={toggleSidebar}>
           <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
         </button>
       </header>
       <SidebarComponent
         width={270}
-        ref={(Sidebar) => (Sidebar = sidebar)}
+        ref={(Sidebar) => (sidebar = Sidebar)}
         created={() => sidebar.hide()}
         closeOnDocumentClick={true}
         showBackdrop={true}
         type="over"
       >
-        <NavItems />
+        <NavItems handleclick={toggleSidebar} />
       </SidebarComponent>
     </div>
   );
