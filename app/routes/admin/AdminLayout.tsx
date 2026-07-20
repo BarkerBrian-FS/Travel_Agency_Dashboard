fe3a8deb0ddd00bf33ab86ba7f91e1a6e45d29e4
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-20 13:33:38 -0500

    debugged and finished sidebar

--- app/routes/admin/AdminLayout.tsx
+++ app/routes/admin/AdminLayout.tsx
@@ -1,14 +1,21 @@
-import React from "react";
 import { Outlet } from "react-router";
+import { NavItems } from "../../../components";
+
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 
 const AdminLayout = () => {
   return (
-    <div className="AdminLayout.tsx">
+    <div className="admin-layout">
+      <aside className="w-full max-w-67.5 hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
+
       <aside className="children">
         <Outlet />
       </aside>
     </div>
   );
 };
-
 export default AdminLayout;
