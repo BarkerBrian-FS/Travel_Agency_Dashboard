111a06842ca4a989c286ccaca06085c0677fb752
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-20 13:37:02 -0500

    starting mobile side bar

--- app/routes/admin/AdminLayout.tsx
+++ app/routes/admin/AdminLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router";
-import { NavItems } from "../../../components";
-
+import { NavItems, MobileSidebar } from "../../../components";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
+      <MobileSidebar />
       <aside className="w-full max-w-67.5 hidden lg:block">
         <SidebarComponent width={270} enableGestures={false}>
           <NavItems />
