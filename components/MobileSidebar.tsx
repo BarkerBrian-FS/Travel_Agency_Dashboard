efefd3ef02c628fa34e95e6fe3e9feba2e8dc9c3
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-20 13:50:25 -0500

    added mobile sidebar with nav items menu needs more work

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,5 +1,37 @@
+//@ts-nocheck
+
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+
+import { Link } from "react-router";
+import NavItems from "./NavItems";
+
 const MobileSidebar = () => {
-  return <div>MobileSidebar</div>;
+  let sidebar: SidebarComponent;
+
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header>
+        <Link to="/">
+          <img src="/assets/icons/logo.svg" alt="logo" className="size-7.5" />
+          <h1>Tourvisto</h1>
+        </Link>
+
+        <button onClick={() => sidebar.toggle()}>
+          <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
+        </button>
+      </header>
+      <SidebarComponent
+        width={270}
+        ref={(Sidebar) => (Sidebar = sidebar)}
+        created={() => sidebar.hide()}
+        closeOnDocumentClick={true}
+        showBackdrop={true}
+        type="over"
+      >
+        <NavItems />
+      </SidebarComponent>
+    </div>
+  );
 };
 
 export default MobileSidebar;
