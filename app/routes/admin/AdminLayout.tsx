34160194629349f4aaadaf884d1f08a950abc9fb
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-22 10:05:08 -0500

    added client loader to admin layout

--- app/routes/admin/AdminLayout.tsx
+++ app/routes/admin/AdminLayout.tsx
@@ -1,6 +1,25 @@
-import { Outlet } from "react-router";
+import { Outlet, redirect } from "react-router";
 import { NavItems, MobileSidebar } from "../../../components";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { account } from "~/appwrite/client";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
+
+export async function clientLoader() {
+  try {
+    const user = await account.get();
+
+    if (!user.$id) redirect("/sign-in");
+
+    const existingUser = await getExistingUser(user.$id);
+    if (existingUser?.status === "user") {
+      return redirect("/");
+    }
+    return existingUser?.id ? existingUser : await storeUserData();
+  } catch (error) {
+    console.log("Error in ClientLoader adminLay", error);
+    return redirect("/sign-in");
+  }
+}
 
 const AdminLayout = () => {
   return (
