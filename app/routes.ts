823277785b0a12d62c5d0c852cec8565ab16ff41
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-24 13:51:17 -0500

    working on gemini integration and unsplash api calls

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
   route("sign-in", "routes/root/sign-in.tsx"),
+  route("api/create-trip", "routes/api/create-trip.ts"),
   layout("routes/admin/AdminLayout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
     route("allUsers", "routes/admin/allUsers.tsx"),
